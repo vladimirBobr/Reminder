6de1fcd854d918aba539e4a6e3c3f64acdd2c120
Бобровский Владимир Сергеевич <bobrovskiivs@sovcombank.ru>
2026-04-10 09:35:24 +0400

    fix warning

--- Reminder/Common/ProxyHelper.cs
+++ Reminder/Common/ProxyHelper.cs
@@ -4,26 +4,20 @@ namespace ReminderApp.EventNotification;
 
 public static class ProxyHelper
 {
-    public static void ConfigProxy()
-    {
-        WebRequest.DefaultWebProxy = CreateProxy();
-    }
-
-    public static WebProxy CreateProxy()
+    public static WebProxy? CreateProxy()
     {
 #if DEBUG
         return null;
 
-        if (OperatingSystem.IsWindows())
-        {
-            var webProxy = new WebProxy("", 9090);
-            webProxy.UseDefaultCredentials = true;
-            return webProxy;
-        }
+        //if (OperatingSystem.IsWindows())
+        //{
+        //    var webProxy = new WebProxy("", 9090);
+        //    webProxy.UseDefaultCredentials = true;
+        //    return webProxy;
+        //}
 
 #else
         
 #endif
-        return null;
     }
 }
